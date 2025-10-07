8a03ad3f87ac28246a9c3efd26308de82e05f7c3
Svajus000 <svajunaskoncius11@gmail.com>
2025-10-07 11:57:27 +0300

    Replace repetitive code and add hover transition

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,21 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
+const links = [
+  { name: "GitHub", url: "https://github.com/" },
+  { name: "Frontend Mentor", url: "https://frontendmentor.io/" },
+  { name: "LinkedIn", url: "https://linkedin.com/" },
+  { name: "Twitter", url: "https://twitter.com/" },
+  { name: "Instagram", url: "https://instagram.com/" },
+];
+
 export default function Home() {
   return (
     <>
-      <div
+      <main
         className={`${inter.className} w-screen min-h-screen bg-[#141414ff] flex justify-center items-center pt-[15vh] pb-[15vh]`}
       >
-        <div className=" w-md h-full bg-[#1f1f1fff] rounded-2xl ">
+        <section className=" w-md h-full bg-[#1f1f1fff] rounded-2xl ">
           <div className="h-1/5 flex justify-center items-center mt-8 mb-4">
             <Image
               className="w-24 rounded-full"
@@ -23,7 +31,7 @@ export default function Home() {
           <div className="h-4/5 flex flex-col items-center">
             <div className="flex flex-col justify-center items-center pb-8">
               <h1 className="text-[#ffffffff] font-bold text-[24px]">
-                <p>Jessica Randall</p>
+                Jessica Randall
               </h1>
               <h2 className="text-[#c5f82aff] text-[14px] font-semibold">
                 London, United Kingdom
@@ -33,29 +41,20 @@ export default function Home() {
               &quot;Front-end developer and avid reader.&quot;
             </p>
             <div className="grid grid-cols-1 gap-y-4 w-full  p-10 pt-4 text-[#ffffffff] text-[14px]">
-              <button className="w-full h-12 bg-[#333333ff] font-semibold rounded-md cursor-pointer hover:bg-[#c5f82aff] hover:text-black active:cursor-[url('/images/black-cursor.png')_auto]">
-                GitHub
-              </button>
-
-              <button className="w-full h-12 bg-[#333333ff] font-semibold rounded-md cursor-pointer hover:bg-[#c5f82aff] hover:text-black hover:cursor-black">
-                Frontend Mentor
-              </button>
-
-              <button className="w-full h-12 bg-[#333333ff] font-semibold rounded-md cursor-pointer hover:bg-[#c5f82aff] hover:text-black ">
-                LinkedIn
-              </button>
-
-              <button className="w-full h-12 bg-[#333333ff] font-semibold rounded-md cursor-pointer hover:bg-[#c5f82aff] hover:text-black ">
-                Twitter
-              </button>
-
-              <button className="w-full h-12 bg-[#333333ff] font-semibold rounded-md cursor-pointer hover:bg-[#c5f82aff] hover:text-black ">
-                Instagram
-              </button>
+              {links.map((link) => {
+                return (
+                  <button
+                    key={link.name}
+                    className="w-full h-12 bg-[#333333ff] font-semibold rounded-md cursor-pointer hover:bg-[#c5f82aff] hover:text-black transition-all duration-200 ease-in-out"
+                  >
+                    {link.name}
+                  </button>
+                );
+              })}
             </div>
           </div>
-        </div>
-      </div>
+        </section>
+      </main>
     </>
   );
 }
